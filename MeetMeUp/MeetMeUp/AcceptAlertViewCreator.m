3c7aa5e1e540e99684908dc90e87749198e2cc11
tnylert <Ty Lert>
2014-08-29 18:39:02 +0100

    dfvdf

--- MeetMeUp/MeetMeUp/AcceptAlertViewCreator.m
+++ MeetMeUp/MeetMeUp/AcceptAlertViewCreator.m
@@ -13,6 +13,7 @@
 #define ALERT_VIEW_COLOR [UIColor colorWithRed:250.0f/255.0f green:63.0f/255.0f blue:82.0f/255.0f alpha:0.90f]
 #define ALERT_VIEW_HIDE_FRAME CGRectMake(17, viewController.view.bounds.origin.y-ALERT_VIEW_HEIGHT, ALERT_VIEW_WIDTH, ALERT_VIEW_HEIGHT)
 #define ALERT_VIEW_SHOW_FRAME CGRectMake(17, viewController.view.bounds.origin.y, ALERT_VIEW_WIDTH, ALERT_VIEW_HEIGHT)
+#define IsIphone5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
 
 @interface AcceptAlertViewCreator()
 {
@@ -25,51 +26,71 @@
 
 - (UIView *) createAlertViewWithViewController:(UIViewController *)viewController andText:(NSString *)alertText
 {
-    alertView = [[UIView alloc] init];
-    [alertView setFrame:ALERT_VIEW_HIDE_FRAME];
+    for (UIView *view in viewController.view.subviews)
+    {
+        [view setUserInteractionEnabled:NO];
+    }
+    
+    UIView *shadowView = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [shadowView setBackgroundColor:[UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.70f]];
     
     UIImageView *alertBGImageView = [[UIImageView alloc] init];
-    [alertBGImageView setImage:[UIImage imageNamed:@"AcceptAlert_BG.png"]];
-    [alertBGImageView setFrame:CGRectMake(17, 25, ALERT_VIEW_WIDTH, ALERT_VIEW_HEIGHT)];
+    UIImage *alertViewBGImage = [UIImage imageNamed:@"AcceptAlert_BG.png"];
+    [alertBGImageView setImage:alertViewBGImage];
+    [alertBGImageView setFrame:CGRectMake((viewController.view.frame.size.width - alertViewBGImage.size.width)/2, 0 - alertViewBGImage.size.height, alertViewBGImage.size.width, alertViewBGImage.size.height)];
     [alertBGImageView setContentMode:UIViewContentModeScaleAspectFit];
+    alertView = [[UIView alloc] init];
+    [alertView setFrame:alertBGImageView.frame];
     [alertView setBackgroundColor:[UIColor colorWithPatternImage:alertBGImageView.image]];
     
-    
-    UILabel *alertViewLabel = [[UILabel alloc] init];
-    [alertViewLabel setFrame:CGRectMake(viewController.view.bounds.origin.x, 15, ALERT_VIEW_WIDTH, ALERT_VIEW_HEIGHT)];
+    UITextView *alertViewLabel = [[UITextView alloc] init];
     [alertViewLabel setTextAlignment:NSTextAlignmentCenter];
-    [alertViewLabel setTextColor:[UIColor whiteColor]];
-    [alertViewLabel setFont:[UIFont systemFontOfSize:13.0f]];
     [alertViewLabel setText:alertText];
+    [alertViewLabel sizeToFit];
+    [alertViewLabel setFrame:CGRectMake(viewController.view.bounds.origin.x + 25, 17, 70, 50)];
+    [alertViewLabel setTextColor:[UIColor colorWithRed:44.0f/255.0f green:44.0f/255.0f blue:44.0f/255.0f alpha:1.0f]];
+    [alertViewLabel setBackgroundColor:[UIColor redColor]];
+    [alertViewLabel setFont:[UIFont systemFontOfSize:13.0f]];
     [alertView addSubview:alertViewLabel];
     
-    CGPoint originalCenter = alertView.center;
+    UIButton *cancelButton = [[UIButton alloc] initWithFrame:CGRectMake(alertBGImageView.frame.origin.x + 7, alertBGImageView.frame.origin.y + 122, 123, 48)];
+    [cancelButton setBackgroundColor:[UIColor clearColor]];
+    [cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    [cancelButton setShowsTouchWhenHighlighted:YES];
+    [cancelButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:19]];
+    [cancelButton setTitleColor:[UIColor colorWithRed:102.0f/255.0f green:102.0f/255.0f blue:102.0f/255.0f alpha:1.0f] forState:UIControlStateNormal];
+    [alertView addSubview:cancelButton];
     
-//    [UIView animateWithDuration:0.3
-//                     animations:^{
-//                         CGPoint center = alertView.center;
-//                         center.y += ALERT_VIEW_HEIGHT;
-//                         alertView.center = center;
-//                     }
-//                     completion:^(BOOL finished){
-//                         [UIView animateWithDuration:0.3 delay:1.5 options:UIViewAnimationOptionTransitionNone animations:^{
-//                             alertView.center = originalCenter;
-//                         } completion:nil];
-//                         
-//                     }];
+    UIButton *acceptButton = [[UIButton alloc] initWithFrame:CGRectMake(alertBGImageView.frame.origin.x + 140, alertBGImageView.frame.origin.y + 122, 123, 48)];
+    [acceptButton setBackgroundColor:[UIColor clearColor]];
+    [acceptButton setShowsTouchWhenHighlighted:YES];
+    [acceptButton setTitle:@"Accept" forState:UIControlStateNormal];
+    [acceptButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:19]];
+    [acceptButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [alertView addSubview:acceptButton];
+    
+    
+    
+    float heightIncrement = 300.0;
     
     [UIView animateWithDuration:0.3
                      animations:^{
-                         CGPoint center = alertView.center;
-                         center.y += ALERT_VIEW_HEIGHT  + 25;
-                         alertView.center = center;
+                         [alertView setFrame:CGRectMake(alertView.frame.origin.x, alertView.frame.origin.y + heightIncrement, alertView.frame.size.width, alertView.frame.size.height)];
+                         [cancelButton setFrame:CGRectMake(cancelButton.frame.origin.x, cancelButton.frame.origin.y + 210, cancelButton.frame.size.width, cancelButton.frame.size.height)];
+                         [acceptButton setFrame:CGRectMake(acceptButton.frame.origin.x, acceptButton.frame.origin.y + 210, acceptButton.frame.size.width, acceptButton.frame.size.height)];
+                         [alertViewLabel setFrame:CGRectMake(viewController.view.bounds.origin.x + 15, 17 + 70, 276 - (15*2), 25)];
                      }
                      completion:^(BOOL finished){
                          
                      }];
+    
+    [shadowView addSubview:alertView];
 
     
-    return alertView;
+    return shadowView;
+    
 }
+
+
 
 @end
